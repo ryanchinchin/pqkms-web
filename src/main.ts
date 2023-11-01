72879c47b2e1f7c3aea1614d54b8900ebcddf2a9
Yogesh Swami <yogesh.swami@securesubstrates.com>
2023-11-01 00:52:42 -0700

    Adding code for login
    Adding code that can perform login

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,102 @@
-import { register_user } from "@securesubstrates/pqkms";
+import {
+  register_user,
+  fetch_user_projects,
+  login_user,
+} from "@securesubstrates/pqkms";
 import "./style.css";
 
-async function sendForm(form: HTMLFormElement) {
+async function sendRegistrationRequest(form: HTMLFormElement): Promise<string> {
   let formData = new FormData(form);
   let domain_prefix = formData.get("domain") as string;
   let email = formData.get("email") as string;
   let password = formData.get("password") as string;
-  console.log(`FormData: ${domain_prefix} => ${email} => ${password}`);
-  await register_user(
-    domain_prefix,
-    email,
+  return register_user(
+    domain_prefix.trim(),
+    email.trim(),
     password,
     "https://registrar.pqkms.dev:8443"
   );
 }
 
-const onSubmit = (event: Event) => {
+async function sendLoginRequest(form: HTMLFormElement) {
+  let formData = new FormData(form);
+  let email = formData.get("email") as string;
+  let password = formData.get("password") as string;
+  let project_list = await fetch_user_projects(
+    "https://registrar.pqkms.dev:8443",
+    email.trim()
+  );
+  login_user(project_list[0], password);
+}
+
+function regDivElement(): HTMLDivElement {
+  return document.querySelector("body > div.registration") as HTMLDivElement;
+}
+
+function regStatusElement(): HTMLDivElement | null {
+  let status_element = document.querySelector(
+    "body > div.registration > div.read-the-docs"
+  ) as HTMLDivElement;
+
+  if (status_element) {
+    return status_element;
+  } else {
+    let reg_elem = regDivElement();
+
+    if (reg_elem) {
+      let status_element = document.createElement("div");
+      reg_elem.appendChild(status_element);
+      return status_element;
+    } else {
+      return null;
+    }
+  }
+}
+
+function setRegStatus(msg: string) {
+  let status = regStatusElement();
+  if (status) {
+    status.innerText = msg;
+  }
+}
+
+const doRegistration = (event: Event) => {
   event.preventDefault();
   if (event.currentTarget) {
-    try {
-      sendForm(event.currentTarget as HTMLFormElement);
-    } catch (e) {
-      alert(`Failed to register user ${e}`);
-    }
-    // .then((response) => response.json())
-    // .then((data) => alert(`Response ID: ${data.id}`))
-    // .catch(() => alert("HTTP Error!"));
+    sendRegistrationRequest(event.currentTarget as HTMLFormElement)
+      .then(setRegStatus)
+      .catch((e) => {
+        setRegStatus(`Registration failed: ${e.message}`);
+      });
+  } else {
+    setRegStatus("Invalid event target for registration form");
+  }
+};
+
+const doLogin = (event: Event) => {
+  event.preventDefault();
+  if (event.currentTarget) {
+    sendLoginRequest(event.currentTarget as HTMLFormElement).catch((e) => {
+      alert(`Login failed: ${e}`);
+    });
   } else {
     alert("Invalid event target for registration form");
   }
 };
 
 const init = () => {
-  const form = document.querySelector("body > div > form");
-  if (form) {
-    form.addEventListener("submit", onSubmit);
-  } else {
-    alert(`Invalid registration test configuration`);
+  const registration_form = document.querySelector(
+    "body > div.registration > form"
+  );
+
+  const login_form = document.querySelector("body > div.login > form");
+
+  if (registration_form) {
+    registration_form.addEventListener("submit", doRegistration);
+  }
+
+  if (login_form) {
+    login_form.addEventListener("submit", doLogin);
   }
 };
 
